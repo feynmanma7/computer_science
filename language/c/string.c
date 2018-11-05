143393d9fff7b773d40a5faa2c0e7a4dd71326fb
wadefall <flyingman26@sina.com>
2018-11-05 20:52:25 +0800

    c programming language
    c programming language

---
+++ language/c/string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+char s[6] = "hello";
+printf("strlen of %s is %d\n", s, strlen(s));
+printf("sizeof of %s is %d\n", s, sizeof(s));
+
+return 0;
+}
